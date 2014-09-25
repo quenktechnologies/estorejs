829581040273290918155d2b1efe9b5a8f802cc1
Lasana Murray <lmurray@quenk.com>
2014-09-25 14:29:08 -0400

    Rename cart count endpoint.

--- core/api/Endpoints.js
+++ core/api/Endpoints.js
@@ -15,7 +15,7 @@ module.exports = function Endpoints() {
 		PRODUCT: new RegExp('^/_/products/' + slug + '$'),
 
 		CART_ITEMS: '/_/cart/items',
-		CART_ITEMS_COUNT: '/_/cart/items/@count',
+		CART_ITEMS_COUNT: '/_/cart/items/count',
 		CART_ITEM: new RegExp('^/_/cart/items/' + slug + '$'),
 		CART_ITEM_QUANTITY: new RegExp('^/_/cart/items/' + slug + '/@quantity$'),
 		CART_ITEM_QUANTITY_VALUE: new RegExp('^/_/cart/items/' + slug + '/@quantity/([\\d]{1,16})$'),
