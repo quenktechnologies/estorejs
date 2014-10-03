41efd49d242d70b83bb8e0a8ab4c67ca17380b29
Lasana Murray <lmurray@quenk.com>
2014-10-03 11:31:05 -0400

    Add some new documentation on extensions.

--- core/util/Extension.js
+++ core/util/Extension.js
@@ -1,14 +1,41 @@
 /**
- * Extension is the base object for all extensions.
+ * Extension acts as the base object of all extensions.
+ *
+ * EStore is built to be extension (plugin) friendly from the start. All of the
+ * internal route bindings are done via extensions and follow the same steps you would
+ * take to include a 3rd party extension.
+ *
+ * The folder `extras/extensions` is scanned on startup for 3rd party extension code.
+ * Extensions should be self contained in their individual folders and are expected to
+ * export a single object. That is `typeof require('myextension`) === 'object'`.
+ *
+ * That object is described below:
+ *
+ * ``` javascript
+ *
+ *   {
+ *
+ *     name:       // <String|Required>    The name/title of the extension. (example: 'Awesome Checkout')
+ *
+ *     settings:   // <Function|Optional>  A function that is called to include the extensions's config in the
+ *                                         Settings model. The function signature is function(settingsArray, fieldTypes).
+ *
+ *     controller: // <Function|Required> This function is the actual extension. It will have it's prototype forced to
+ *                                        the Extension object. All extensions are instantiated with a reference to
+ *                                        the main EStore object.
+ *
+ *   }
+ *
+ *
+ *
+ * ```
+ *
  * @class Extension
  * @param {Estore} store
  * @constructor
  *
  */
 module.exports = function Extension(store) {
-
-
-
 
 	/**
 	 * onRouting is called to setup routes.
@@ -39,18 +66,18 @@ module.exports = function Extension(store) {
 
 	};
 
-        /**
-         * onGateways 
-         *
-         * @method onGateways
-         * @return 
-         *
-         */
-        this.onGateways = function () {
+	/**
+	 * onGatewayRegistration
+	 *
+	 * @method onGatewayRegistration
+	 * @return
+	 *
+	 */
+	this.onGatewayRegistration = function() {
 
 
 
-        };
+	};
 
 
 
