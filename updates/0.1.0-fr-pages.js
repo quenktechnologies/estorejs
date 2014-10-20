4f84535e14478ec6677ea18e0d38127a5d9d639c
Lasana Murray <lmurray@quenk.com>
2014-10-20 14:11:13 -0400

    Clean up default template a bit.

--- updates/0.1.0-fr-pages.js
+++ updates/0.1.0-fr-pages.js
@@ -9,9 +9,15 @@ exports.create = {
 
 			title: "Frontpage",
 			template: "pages/default.html",
-			"content.md": "#E-Commerce with Nodejs and MongoDB\n" +
-				"This page is marked as the index, so it's content " +
-				"will be loaded. To change this go [here](/keystone/pages)",
+			"content.md": "##Welcome!\n" +
+				"If you are seeing this, you install of E-Store " +
+				"should be working!\n\n Pat yourself on the back, " +
+				"you deserve it! There is more " +
+				"to be done however! Start by logging into " +
+				"[keystone](/keystone) and editing the content " +
+				"of this page.\n\n" +
+				"<a href=\"/keystone\" target=\"_blank\" class=\"btn btn-danger" +
+				"\">Log in</a>",
 			"index": true
 		}
 
