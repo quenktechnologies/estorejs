3b7940e359de6712360bda01b0680f428d00e24e
Lasana Murray <lmurray@quenk.com>
2014-11-01 23:45:49 -0400

    Use keywords.

--- updates/0.1.0-fr-products.js
+++ updates/0.1.0-fr-products.js
@@ -10,7 +10,7 @@ exports.create = {
 			"order.min": 1,
 			"order.max": 3,
 			isFeatured: true,
-			_keywords: "samsung,galaxy,android,phone,smartphone,test",
+			keywords: ["samsung", "galaxy", "android", "phone", "smartphone", "test"],
 			"description.short.md": "This product was created as a test.",
 			"description.long.md": "This is the long description of this product."
 
@@ -25,8 +25,7 @@ exports.create = {
 			"order.min": 1,
 			"order.max": 3,
 			isFeatured: true,
-			_keywords: "nokia,cellphone,phone,smartphone,test",
-
+			keywords: ["nokia", "cellphone", "phone", "smartphone", "test"],
 			description: {
 				short: "This product was created as a test.",
 				long: {
@@ -41,11 +40,11 @@ exports.create = {
 			name: "Generic Case",
 			price: "5.25",
 			image: "/assets/images/product3.jpg",
-					"stock.balance": 100,
+			"stock.balance": 100,
 			"order.min": 1,
 			"order.max": 3,
 			isFeatured: true,
-			_keywords: "case,phones,cellphone,test",
+			keywords: ["case", "phones", "cellphone", "test"],
 			order: {
 				min: 1,
 				max: 30
@@ -60,11 +59,11 @@ exports.create = {
 			name: "Sony Phone R",
 			price: "53.99",
 			image: "/assets/images/product4.jpg",
-					"stock.balance": 100,
+			"stock.balance": 100,
 			"order.min": 1,
 			"order.max": 3,
 			isFeatured: true,
-			_keywords: "sony,cellphone,test",
+			keywords: ["sony", "cellphone", "test"],
 			order: {
 				min: 1,
 				max: 3
@@ -78,11 +77,11 @@ exports.create = {
 			name: "Very Generic Case",
 			price: "15.25",
 			image: "/assets/images/product3.jpg",
-					"stock.balance": 100,
+			"stock.balance": 100,
 			"order.min": 1,
 			"order.max": 3,
 			isFeatured: true,
-			_keywords: "case,phones,cellphone,test",
+			keywords: ["case", "phones", "cellphone", "test"],
 			order: {
 				min: 1,
 				max: 30
