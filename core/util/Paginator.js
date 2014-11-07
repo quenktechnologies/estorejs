d664cf32ef8dd151b7381eda4549a6d5e6d945f4
Lasana Murray <lmurray@quenk.com>
2014-11-07 16:54:05 -0400

    Object for simple pagination.

---
+++ core/util/Paginator.js
@@ -0,0 +1,68 @@
+/**
+ * Paginator provides an api for paging list type pages.
+ * @class Paginator
+ * @param {Object} model The model to page.
+ * @param {Number} maxItems The max number of items to display on a page.
+ * @constructor
+ *
+ */
+module.exports = function Paginator(model, maxItems) {
+
+
+	/**
+	 * paginate performs the actual pagination logic.
+	 *
+	 * @method paginate
+	 * @param {Number} current
+	 * @param {Object} query
+	 * @return {Promise} mongoose style
+	 *
+	 */
+	this.paginate = function(current, query) {
+
+		query = query || {};
+
+		var pager = {
+			total: {},
+			pages: [],
+		};
+
+		return model.
+		find().
+		lean().
+		count().
+		exec().
+		then(function(count) {
+
+			pager.total.items = count;
+			pager.total.pages = Math.ceil(count / maxItems);
+			pager.current = current;
+			pager.next = current + 1;
+			pager.previous = current - 1;
+
+			if (pager.current + 1 >= pager.total.pages)
+			//This is the last page
+				pager.next = -1;
+
+			if (pager.current === 0)
+			//This is the first page.
+				pager.previous = -1;
+
+			for (var i = 0; i < pager.total.pages; i++)
+				pager.pages.push(i);
+
+			return model.find(query).skip(current * maxItems).limit(maxItems).lean().exec();
+
+		}).
+		then(function(results) {
+			pager.items = results;
+			return pager;
+		});
+
+
+
+	};
+
+
+
+};
