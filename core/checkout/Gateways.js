002622066afd34f66d8eb8cae933f2ea2abcf69d
Lasana Murray <lmurray@quenk.com>
2014-11-09 01:41:47 -0400

    Make Gateways its own object.

---
+++ core/checkout/Gateways.js
@@ -0,0 +1,29 @@
+/**
+ * Gateways is a map of gateways we know about.
+ * @class Gateways
+ * @param {gateways} gateways
+ * @constructor
+ *
+ */
+module.exports = function Gateways() {
+
+	this.available = [];
+	this.other = [];
+	this.active = {};
+	this.list = [];
+
+	/**
+	 * hasActive checks if the given key is an active gateway.
+	 *
+	 * @method hasActive
+	 * @param {String} key
+	 * @return
+	 *
+	 */
+	this.hasActive = function(key) {
+
+		return this.active.hasOwnProperty(key);
+	};
+
+
+};
