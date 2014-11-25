e7f8761b455ba95f726e9e032d7c4c113cbdd031
Lasana Murray <lmurray@quenk.com>
2014-11-25 17:38:34 -0400

    Add extra pages scripts

---
+++ updates/pages/index.js
@@ -0,0 +1,10 @@
+module.exports =
+	"##Welcome!\n" +
+	"If you are seeing this, you install of E-Store " +
+	"should be working!\n\n Pat yourself on the back, " +
+	"you deserve it! There is more " +
+	"to be done however! Start by logging into " +
+	"[keystone](/keystone) and editing the content " +
+	"of this page.\n\n" +
+	"<a href=\"/keystone\" target=\"_blank\" class=\"btn btn-danger" +
+	"\">Log in</a>";
